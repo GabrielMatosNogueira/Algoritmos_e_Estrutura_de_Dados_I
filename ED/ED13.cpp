7c94ffb104cadeb1f3265cb732114ed77ba08366
Gabriel Matos Nogueira <1563147@sga.pucminas.br>
2025-06-03 16:50:43 -0300

    Update ED13.cpp

--- ED/ED13.cpp
+++ ED/ED13.cpp
@@ -1,4 +1,38 @@
 #include "io.hpp"
+#include "ED13.hpp"
+#include <iostream>
+#include <stdio.h>
+
+using namespace std;
+
+void metodo01 ()
+{
+	contato contato1;
+	string name = "";
+	
+	cout << endl << "Digite o nome do contato"
+	" (sem espacos, utilize '_' entre nome e sobrenomes): ";
+
+	cin >> name;
+	cout << endl;	
+
+	contato1.setName(name);
+	contato1.showName();
+	cout << endl;
+
+	getchar();
+}
+
+void metodo02 ()
+{
+	string telefone="";
+	contato contato1;
+	
+	cout << endl;
+	telefone = IO_readstring("Digite o numero de telefone desejado: ");
+	contato1.setPhone(telefone);
+	contato1.showPhone();
+}
 
 int main ( void )
 {
@@ -7,6 +41,17 @@ int main ( void )
 	{
 		IO_methods(13);
 		opcao=IO_readint("\nDigite a opcao desejada: ");
+		switch(opcao)
+		{
+			case 1:
+			metodo01();
+			break;
+
+			case 2:
+			metodo02();
+			break;
+		}
+
 	}while (opcao!=0);
 	
 	return 0;
