002fbbad610c9145af8391673489d03ac85e7610
Gabriel Matos Nogueira <1563147@sga.pucminas.br>
2025-06-08 17:25:09 -0300

    Update ED13.hpp

--- Biblioteca/ED13.hpp
+++ Biblioteca/ED13.hpp
@@ -13,6 +13,8 @@ private:
     string nome;
     string telefone;
     string telefone2;
+    string enderecoResidencial;
+    string enderecoComercial;
     int quantidadeTelefones;
     bool OK;
 
@@ -22,8 +24,10 @@ public:
         this->nome = "";
         this->telefone = "";
         this->telefone2 = "";
+        this->enderecoResidencial = "";
+        this->enderecoComercial = "";
         this->OK = false;
-        this->quantidadeTelefones=0;
+        this->quantidadeTelefones = 0;
     }
 
     void setName(const string name)
@@ -98,7 +102,8 @@ public:
             // Verifica se todos os outros caracteres são dígitos
             for (int i = 0; i < tamanho; ++i)
             {
-                if (i == 7) continue; // Posição do '-'
+                if (i == 7)
+                    continue; // Posição do '-'
                 if (!isdigit(phone[i]))
                 {
                     cout << endl
@@ -151,12 +156,11 @@ public:
         cout << endl
              << "Telefone 2 lido: " << this->telefone2 << endl;
     }
-    
 
     void readFromFile(const string filename)
     {
-        ifstream arquivo (filename);
-        if(arquivo.is_open())
+        ifstream arquivo(filename);
+        if (arquivo.is_open())
         {
             arquivo >> this->nome;
             arquivo.close();
@@ -165,23 +169,113 @@ public:
         {
             IO_print("\nErro ao abrir o arquivo.");
         }
-
     }
 
     void getReadNameFromFile(void)
     {
-        cout << endl << "Nome lido do arquivo: " << this->nome;
+        cout << endl
+             << "Nome lido do arquivo: " << this->nome;
     }
 
-    void writeToFile (const string filename)
+    void writeToFile(const string filename)
     {
         string nome;
-        ofstream arquivo (filename);
+        ofstream arquivo(filename);
 
-        nome=IO_readstring("\nDigite o nome que deseja guardar no arquivo: ");
+        nome = IO_readstring("\nDigite o nome que deseja guardar no arquivo: ");
 
         arquivo << nome;
 
         arquivo.close();
+    }
+
+    int phones(void)
+    {
+        if (this->telefone2.empty())
+        {
+            this->quantidadeTelefones = 1;
+        }
+        else
+        {
+            this->quantidadeTelefones = 2;
+        }
+        return this->quantidadeTelefones;
+    }
+
+    void setPhone2a(const string phone2)
+    {
+        if (phone2 == "")
+        {
+            cout << "Numero de telefone vazio." << endl;
+        }
+        else
+        {
+            this->telefone2 = phone2;
+            this->quantidadeTelefones = quantidadeTelefones + 1;
+        }
+    }
+
+    void showPhone2a(void)
+    {
+        cout << endl
+             << "Segundo telefone lido: " << this->telefone2 << endl;
+    }
+
+    void setPhone2b(const string phone2)
+    {
+        bool verificador = false;
+
+        verificador = IO_readbool("\nDeseja alterar o segundo numero de telefone? (1 = Sim / 0 = Nao): ");
+        if (verificador)
+        {
+            if (this->quantidadeTelefones < 2)
+            {
+                IO_print("\nErro: Contato nao possui dois numeros de telefone.");
+            }
+            else
+            {
+                this->telefone2 = phone2;
+                this->quantidadeTelefones++;
+                cout << "\nSegundo telefone alterado com sucesso!" << endl;
+            }
+        }
+        else
+        {
+            cout << "\nNenhuma alteracao selecionada" << endl;
+        }
+    }
+
+    void addPhone(const string phone)
+    {
+        if (this->quantidadeTelefones < 10)
+        {
+            this->setPhone2(phone);
+        }
+        else
+        {
+            cout << "\nLimite de telefones atingido. Nao e' possivel adicionar mais." << endl;
+        }
+    }
+
+    void setEnderecoResidencial(const string endereco)
+    {
+        this->enderecoResidencial = endereco;
+    }
+
+    void getEnderecoResidencial(void)
+    {
+        cout << endl
+             << "Endereco residencial lido: " << this->enderecoResidencial << endl;
+    }
+
+    void setEnderecoComercial(const string endereco)
+    {
+        this->enderecoComercial = endereco;
+    }
+
+    void getEnderecoComercial(void)
+    {
+        cout << endl
+             << "Endereco comercial lido: " << this->enderecoComercial << endl;
     }
 };
