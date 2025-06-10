fad3cab7f273848728660144c85f4d700497536a
Gabriel Matos Nogueira <1563147@sga.pucminas.br>
2025-06-10 19:13:30 -0300

    Create ED14.hpp

---
+++ Biblioteca/ED14.hpp
@@ -0,0 +1,113 @@
+/*
+0870863_Gabriel_Matos_Nogueira
+ED14
+*/
+
+/*
+DEPENDENCIAS/BIBLIOTECAS
+*/
+#include "io.hpp"
+using namespace std;
+
+
+/*
+CLASSE ED14
+
+Classe necessaria para realizar a tarefa "Estudo Dirigido 14"
+*/
+class ED14
+{
+private:
+    int numero_inteiro;
+    double numero_real;
+    string cadeia_caracteres;
+
+public:
+    /*
+    CONSTRUTOR ED14
+    
+    Responsavel por criar e inicializar as variaveis a qual inicializa as variaveis 
+    utilizadas no metodo private da classe ED14
+    */
+    ED14()
+    {
+        this->numero_inteiro=0;
+        this->numero_real=0.0;
+        this->cadeia_caracteres="";
+    }
+    /*
+    DESTRUTOR ED14
+
+    Responsavel por desatribuir e desreferenciar atributos da classe privada de ED14
+    */
+    ~ED14()
+    {
+        this->numero_inteiro=0;
+        this->numero_real=0.0;
+        this->cadeia_caracteres="";
+    }
+    
+    /*
+    METODO SETINT
+
+    Responsavel por verificar a existencia de um valor inteiro disponivel para uso e,
+    caso exista, atribuir um sinal positivo ou negativo ao mesmo
+
+    @param valor inteiro para o objeto
+    @return numero atribuido ou -1 (em caso de inexistencia)
+    */
+    int setInt(int valor)
+    {
+        if(this->numero_inteiro)
+        {this->numero_inteiro=valor;return valor;}
+        else{return -1;}
+    }
+
+    /*
+    METODO SETDOUBLE
+
+    Responsavel por verificar a existencia de um valor real disponivel para uso e,
+    caso exista, atribuir um sinal positivo ou negativo ao mesmo
+
+    @param valor real para o objeto
+    @return numero atribuido ou -1 (em caso de inexistencia)
+    */
+    int setDouble(double valor)
+    {
+        if(this->numero_real)
+        {this->numero_real=valor;return valor;}
+        else{return -1;}
+    }
+
+    /*
+    METODO SETSTRING
+
+    Responsavel por verificar a existencia de uma string disponivel para uso e,
+    caso exista, atribuir um uma cadeia de caracteres a ela
+
+    @param cadeia de caracteres
+    @return frase atribuida ou -1 (em caso de inexistencia)
+    */
+    string setString(string frase)
+    {
+        if(this->cadeia_caracteres)
+        {this->cadeia_caracteres=frase;return cadeia_caracteres;}
+        else{IO_println("Erro ao obter a frase");}
+    }
+
+    /*
+    METODO GETINT
+
+    Responsavel por verificar a existencia de um valor inteiro e, caso exista,
+    atribuir um sinal positivo ou negativo ao mesmo
+
+    @param vazio
+    @return numero atribuido ou -1 (em caso de inexistencia)
+    */
+    int getInt()
+    {
+        if(this->numero_inteiro)
+        {return numero_inteiro;}
+        else{return -1;}
+    }
+};
