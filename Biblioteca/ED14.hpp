613c2de804f1ddb2243f1a7a522f82483e21d5a0
Gabriel Matos Nogueira <1563147@sga.pucminas.br>
2025-06-15 16:01:24 -0300

    Update ED14.hpp

--- Biblioteca/ED14.hpp
+++ Biblioteca/ED14.hpp
@@ -19,15 +19,10 @@ class ED14
 private:
     int numero_inteiro;
     double numero_real;
+    bool numero_logico;
     string cadeia_caracteres;
 
 public:
-    /*
-    CONSTRUTOR ED14
-
-    Responsavel por criar e inicializar as variaveis a qual inicializa as variaveis 
-    utilizadas no metodo private da classe ED14
-    */
     ED14()
     {
         this->numero_inteiro = 0;
@@ -35,11 +30,6 @@ public:
         this->cadeia_caracteres = "";
     }
 
-    /*
-    DESTRUTOR ED14
-
-    Responsavel por desatribuir e desreferenciar atributos da classe privada de ED14
-    */
     ~ED14()
     {
         this->numero_inteiro = 0;
@@ -47,18 +37,9 @@ public:
         this->cadeia_caracteres = "";
     }
 
-    /*
-    METODO SETINT
-
-    Responsavel por verificar a existencia de um valor inteiro disponivel para uso e,
-    caso exista, atribuir um sinal positivo ou negativo ao mesmo
-
-    @param valor inteiro para o objeto
-    @return numero atribuido ou -1 (em caso de inexistencia)
-    */
     int setInt(int valor)
     {
-        if (this->numero_inteiro!=-1)
+        if (this->numero_inteiro != -1)
         {
             this->numero_inteiro = valor;
             return valor;
@@ -69,18 +50,9 @@ public:
         }
     }
 
-    /*
-    METODO SETDOUBLE
-
-    Responsavel por verificar a existencia de um valor real disponivel para uso e,
-    caso exista, atribuir um sinal positivo ou negativo ao mesmo
-
-    @param valor real para o objeto
-    @return numero atribuido ou -1 (em caso de inexistencia)
-    */
     int setDouble(double valor)
     {
-        if (this->numero_real!=-1.0)
+        if (this->numero_real != -1.0)
         {
             this->numero_real = valor;
             return valor;
@@ -91,59 +63,36 @@ public:
         }
     }
 
-    /*
-    METODO SETSTRING
-
-    Responsavel por verificar a existencia de uma string disponivel para uso e,
-    caso exista, atribuir um uma cadeia de caracteres a ela
-
-    @param cadeia de caracteres
-    @return frase atribuida ou -1 (em caso de inexistencia)
-    */
-    string setString(string frase)
+    int setString(const string& valor)
     {
-        if (this->cadeia_caracteres != "")
+        if (!valor.empty())
         {
-            this->cadeia_caracteres = frase;
-            return cadeia_caracteres;
+            this->cadeia_caracteres = valor;
+            return 1; // Sucesso
         }
         else
         {
-            IO_println("Erro ao obter a frase");
+            IO_println("Cadeia de caracteres vazia");
+            return -1; // Falha
         }
     }
 
-    /*
-    METODO GETINT
-
-    Responsavel por retornar o valor inteiro, caso exista
-
-    @param vazio
-    @return numero atribuido ou -1 (em caso de inexistencia)
-    */
     int getInt()
     {
-        if (this->numero_inteiro!=-1)
+        if (this->numero_inteiro != 0)
         {
             return this->numero_inteiro;
         }
         else
         {
+            IO_println("Erro ao converter para inteiro");
             return -1;
         }
     }
 
-    /*
-    METODO GETDOUBLE
-
-    Responsavel por retornar o valor real, caso exista
-
-    @param vazio
-    @return numero atribuido ou -1 (em caso de inexistencia)
-    */
     double getDouble()
     {
-        if (this->numero_real!=-1.0)
+        if (this->numero_real != -1.0)
         {
             return this->numero_real;
         }
@@ -153,14 +102,6 @@ public:
         }
     }
 
-    /*
-    METODO GETSTRING
-
-    Responsavel por retornar a cadeia de caracteres, caso exista
-
-    @param vazio
-    @return frase atribuida ou mensagem de erro
-    */
     string getString()
     {
         if (!this->cadeia_caracteres.empty())
@@ -172,4 +113,314 @@ public:
             return "[String inexistente]";
         }
     }
+
+    bool getBoolean()
+    {
+        return this->numero_logico;
+    }
+
+    int setBoolean(const std::string& valor)
+    {
+        if (valor == "true" || valor == "T" || valor == "1")
+        {
+            this->numero_logico = true;
+            return 1;
+        }
+        else if (valor == "false" || valor == "F" || valor == "0")
+        {
+            this->numero_logico = false;
+            return 0;
+        }
+        else
+        {
+            IO_println("Valor logico invalido");
+            return -1;
+        }
+    }
+
+    int setBoolean(bool valor)
+    {
+        this->numero_logico = valor;
+        return valor ? 1 : 0;
+    }
+
+    int setBoolean(int valor)
+    {
+        if (valor == 0)
+        {
+            this->numero_logico = false;
+            return 0;
+        }
+        else if (valor == 1)
+        {
+            this->numero_logico = true;
+            return 1;
+        }
+        else
+        {
+            IO_println("Valor logico invalido");
+            return -1;
+        }
+    }
+
+    int setBoolean(char valor)
+    {
+        if (valor == 'T' || valor == '1')
+        {
+            this->numero_logico = true;
+            return 1;
+        }
+        else if (valor == 'F' || valor == '0')
+        {
+            this->numero_logico = false;
+            return 0;
+        }
+        else
+        {
+            IO_println("Valor logico invalido");
+            return -1;
+        }
+    }
+
+    /** 
+     * @brief METODO04
+     * @objective: Funcao para verificar se o parametro (subtexto) esta’ contido no conteudo.
+     *             Com a funcao bool contains ( std::string texto, std::string subtexto )
+     * 
+     */
+    bool contains(const string& texto, const string& subtexto) const
+    {
+        if (texto.empty() || subtexto.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return false;
+        }
+        // Verifica se o subtexto está contido no texto
+        return texto.find(subtexto) != string::npos;
+    }
+    /*
+    Metodo toUpperCase
+    * @brief Converte todas as letras da cadeia de caracteres para maiúsculas.
+    * @return cadeia de caracteres convertida para maiúsculas
+    */
+    string toUpperCase(const string& texto)
+    {
+        if (texto.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return "Falha ao converter para maiusculas";
+        }
+        string upperCaseStr = texto;
+        for (size_t i = 0; i < upperCaseStr.length(); ++i)
+        {
+            char c = upperCaseStr[i];
+            // Se for letra minúscula ('a' a 'z'), converte para maiúscula
+            if (c >= 'a' && c <= 'z')
+            {
+                upperCaseStr[i] = c - ('a' - 'A');
+            }
+        }
+        return upperCaseStr;
+    }
+
+    // Sobrecarga para usar a string interna da classe
+    string toUpperCase()
+    {
+        return toUpperCase(this->cadeia_caracteres);
+    }
+
+    /*
+    Metodo toLowerCase
+    * @brief Converte todas as letras da cadeia de caracteres para minúsculas.
+    * @return cadeia de caracteres convertida para minúsculas
+    */
+    string toLowerCase(const string& texto)
+    {
+        if (texto.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return "";
+        }
+        string lowerCaseStr = texto;
+        for (size_t i = 0; i < lowerCaseStr.length(); ++i)
+        {
+            char c = lowerCaseStr[i];
+            if (c >= 'A' && c <= 'Z')
+            {
+                lowerCaseStr[i] = c + ('a' - 'A');
+            }
+        }
+        return lowerCaseStr;
+    }
+
+    /**
+     * @brief METODO07
+     * 
+     * @objective: Funcao para trocar todas as ocorrencias de certo caractere por outro novo.
+     *             com a funcao replace(char caractere, char novo_caractere)
+     * 
+     * @param caractere - caractere a ser substituido
+     * @param novo_caractere - novo caractere a ser inserido
+     * @return true se a substituição for bem-sucedida, false caso contrário
+     */
+    bool replace(char caractere, char novo_caractere)
+    {
+        if (this->cadeia_caracteres.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return false;
+        }
+        size_t pos = this->cadeia_caracteres.find(caractere);
+        if (pos != std::string::npos)
+        {
+            while (pos != std::string::npos)
+            {
+                this->cadeia_caracteres[pos] = novo_caractere;
+                pos = this->cadeia_caracteres.find(caractere, pos + 1);
+            }
+            return true;
+        }
+        else
+        {
+            return false; // Caractere não encontrado
+        }
+    }
+
+    /**
+     * @brief METODO 08
+     * 
+     * @objective: Funcao para codificar o conteudo segundo a cifra de César (pesquisar)
+     * @param int chave - chave de deslocamento para a cifra, string frase - texto a ser cifrado
+     * @return  valor equivalente codificado, se houver; 
+     *          o proprio valor, caso contrario 
+     */
+    string encrypt(int chave, const string& frase)
+    {
+        if (frase.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return "Erro ao cifrar a frase";
+        }
+        string encryptedStr = frase;
+        for (size_t i = 0; i < encryptedStr.length(); ++i)
+        {
+            char c = encryptedStr[i];
+            // Verifica se é uma letra
+            if (isalpha(c))
+            {
+                char base;
+                if (islower(c))
+                {
+                    base = 'a';
+                }
+                else
+                {
+                    base = 'A';
+                }
+                // Aplica a cifra de César
+                encryptedStr[i] = (c - base + chave + 26) % 26 + base;
+            }
+        }
+        return encryptedStr;
+    }
+
+    /**
+     * @brief METODO09
+     * 
+     * @objective: Funcao para decodificar o conteudo previamente cifrado pela funcao acima.
+     *             com a funcao decrypt(void)
+     * 
+     * @param void
+     * @return valor equivalente decodificado, se houver; o proprio valor, caso contrario 
+     */
+    string decrypt(int chave)
+    {
+        if (this->cadeia_caracteres.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return "";
+        }
+        string decryptedStr = this->cadeia_caracteres;
+        for (size_t i = 0; i < decryptedStr.length(); ++i)
+        {
+            char c = decryptedStr[i];
+            // Verifica se é uma letra
+            if (isalpha(c))
+            {
+                char base;
+                if (islower(c))
+                {
+                    base = 'a';
+                }
+                else
+                {
+                    base = 'A';
+                }
+                // Aplica a cifra de César inversa
+                decryptedStr[i] = (c - base - chave + 26) % 26 + base;
+            }
+        }
+        return decryptedStr;
+    }
+
+    /**
+     * @brief METODO 10
+     * 
+     * @objective: Funcao para verificar se o conteudo esta vazio.
+     * @param void
+     * @return true se estiver vazio, false caso contrario
+     */
+    bool isEmpty() const
+    {
+        if (this->cadeia_caracteres.empty())
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    /**
+     * @brief METODO 11
+     * 
+     * @objective: Funcao para verificar se o conteudo esta vazio.
+     * @param void
+     * @return true se estiver vazio, false caso contrario
+     */
+    bool isNotEmpty() const
+    {
+        if (!this->cadeia_caracteres.empty())
+        {
+            return true;
+        }
+        else
+        {
+            IO_println("Cadeia de caracteres vazia");
+            return false;
+        }
+    }
+    
+    /**
+     * @brief METODO 12
+     * 
+     * @objective: Funcao para verificar se o conteudo é um número inteiro.
+     * @param void
+     * @return true se for um número inteiro, false caso contrário
+     */
+    bool isInteger() const
+    {
+        if (this->numero_inteiro != 0)
+        {
+            return true;
+        }
+        else
+        {
+            IO_println("O conteudo nao e um numero inteiro");
+            return false;
+        }
+    }
+
 };
