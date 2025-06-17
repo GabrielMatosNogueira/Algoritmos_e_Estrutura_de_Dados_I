af17148132825432bb88419f4764796643301548
Gabriel Matos Nogueira <1563147@sga.pucminas.br>
2025-06-17 15:33:36 -0300

    Create LeetCode001.c

---
+++ LeetCode001.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int quantidade_Numeros=0;
+    int i=0;
+    int soma=0;
+    int menor=100000;
+    int target=0;
+    
+    scanf("%d", &quantidade_Numeros);
+    scanf("%d", &target);
+
+    int array[quantidade_Numeros];
+
+    for(i=0; i<quantidade_Numeros; i=i+1)
+    {
+        scanf("%d", &array[i]);
+        getchar();
+    }
+    
+    for(i=0; i<quantidade_Numeros; i=i+1)
+    {
+        if(array[i]<menor)
+        {
+            
+        }
+        else
+        {
+
+        }
+    }
+
+
+
+
+}
